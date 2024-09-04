c84c29ce2ac0f230ee0ee0bb510666591328cefc
dam030 <yun99hj@gmail.com>
2024-09-04 12:52:35 +0900

    1

--- react/my-new-app/src/App.js
+++ react/my-new-app/src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Post from './Post';
+import { useRef, useState } from 'react';
 
 function App() {
   const element = <h1 className='title'>안녕하세요 h1입니다</h1>
@@ -15,9 +16,36 @@ function App() {
       text : '부탁드립니다'
     }]
 
+    const [ number , setNumber] = useState(0);
+    const handleIncressNumber = ()=> {
+      setNumber(number+1)
+    }
+
+    const [text, setText] = useState('');
+    const handleChangeText = (e) => {
+      setText(e.target.value)
+    }
+
+    const [imputText, setInputText] = useState('');
+    const inputFocus = useRef()
+    const handleChangeTextToButton = () => {
+      setInputText(inputFocus.current.value)
+    }
+
   return (
     <div className="App">
       {element}
+
+      <span>{number}</span>
+      <button onClick={handleIncressNumber}>딸깍</button>
+
+      <input type="text" onChange={handleChangeText}/>
+      <p>당신의 입력 : {text}</p>
+
+      <input type='text' ref={inputFocus}/>
+      <button onClick={handleChangeTextToButton}>인풋딸깍</button>
+      <p>버튼 누르면 : {imputText}</p>
+
       {data.map((value,key)=>{
         return <p>{value.title}</p>
       })}
@@ -25,6 +53,7 @@ function App() {
         <p>{value.title}</p>
       )} 
       위와 같은 서식 */}
+
 
       {data.map((value,key)=>
         <Post 제목={value.title} 내용={value.text}></Post>
